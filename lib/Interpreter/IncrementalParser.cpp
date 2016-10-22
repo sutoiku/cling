df726a6bb66563d5d541899d46d95cc1be40d1fb
Yves Le Maout <yves@sutoiku.com>
2016-10-22 13:33:05 -0400

    [Experimental] Hack the IncrementalParser for our needs

--- lib/Interpreter/IncrementalParser.cpp
+++ lib/Interpreter/IncrementalParser.cpp
@@ -777,7 +777,7 @@ namespace cling {
     }
   }
 
-  void IncrementalParser::SetTransformers(bool isChildInterpreter) {
+  void IncrementalParser::SetTransformers(bool isChildInterpreter) { /*
     // Add transformers to the IncrementalParser, which owns them
     Sema* TheSema = &m_CI->getSema();
     // Register the AST Transformers
@@ -800,7 +800,7 @@ namespace cling {
     WrapperTransformers.emplace_back(new CheckEmptyTransactionTransformer(TheSema));
 
     m_Consumer->SetTransformers(std::move(ASTTransformers),
-                                std::move(WrapperTransformers));
+                                std::move(WrapperTransformers)); */
   }
 
 
